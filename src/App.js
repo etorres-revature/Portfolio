89ae862f47039c2c4507bbb81cf68d367a4a681d
Eric D. Torres <etorresnotary@gmail.com>
2020-12-09 11:18:55 -0600

    navbar component created with functional pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-      <h1>HELLO WORLD!!</h1>
+        <h1>HELLO WORLD!!</h1>
       </header>
     </div>
   );
